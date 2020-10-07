9b101fa947b75ee1506876dffd9f0e4eca977d8b
Qudsy <ucisolahuddin@gmail.com>
2020-10-07 13:55:42 +0700

    add

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,10 +6,12 @@ import {
   fetchPersons,
   fetchTopratedMovie,
 } from "../../service";
-import RBCarousel from "react-bootstrap-carousel";
+
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import { Link } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RBCarousel from "react-bootstrap-carousel";
 
 export function Home() {
   const [nowPlaying, setNowPlaying] = useState([]);
@@ -38,18 +40,6 @@ export function Home() {
       <div style={{ height: 500, width: "100%" }} key={index}>
         <div className="carousel-center">
           <img style={{ height: 712, width: 1961 }} src={item.backPoster} alt={item.title} />
-        </div>
-        <div className="carousel-center">
-          <i
-            className="far fa-play-circle"
-            style={{ fontSize: 95, color: "#f4c10f" }}
-          ></i>
-        </div>
-        <div
-          className="carousel-caption"
-          style={{ textAlign: "center", fontSize: 35 }}
-        >
-          {item.title}
         </div>
       </div>
     );
@@ -86,9 +76,7 @@ export function Home() {
       </div>
     );
   });
-
- 
-
+  
   return (
     <div className="container">
       <div className="row mt-2">
@@ -121,75 +109,7 @@ export function Home() {
       <div className="row mt-3">{movieList}</div>
 
       <hr className="mt-5" style={{ borderTop: "1px solid #5a606b" }}></hr>
-
-      <div className="row mt-3 mb-5">
-        <div className="col-md-8 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>ABOUT ME</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-            error earum perspiciatis praesentium sint ipsum provident blanditiis
-            pariatur necessitatibus voluptas, cum, atque iste eligendi autem,
-            culpa cupiditate placeat facilis repellat.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-            perspiciatis? Numquam, enim illo voluptatum neque facere aut sed ut
-            dolore nihil? Nulla sit, recusandae ea tenetur rerum deserunt sequi
-            earum?
-          </p>
-          <ul className="list-inline">
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-youtube"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-md-4 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>KEEP IN TOUCH</h3>
-          <ul className="list-unstyled">
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Address:
-                </strong>{" "}
-                city, state, country
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Phone:
-                </strong>{" "}
-                +01 00 00 00
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-envelope"></i> Email:
-                </strong>{" "}
-                info@infomail.com
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
+      
     </div>
   );
 }
--- src/components/moviedetail/MovieDetail.jsx
+++ src/components/moviedetail/MovieDetail.jsx
@@ -10,6 +10,7 @@ import { Modal } from "react-bootstrap";
 import ReactPlayer from "react-player";
 import ReactStars from "react-rating-stars-component";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function MovieDetail({ match }) {
   let params = match.params;
@@ -45,7 +46,7 @@ export function MovieDetail({ match }) {
         <Modal.Header closeButton>
           <Modal.Title
             id="contained-modal-title-vcenter"
-            style={{ color: "#000000", fontWeight: "bolder" }}
+            style={{  color: "#000000", fontWeight: "bolder" }}
           >
             {detail.title}
           </Modal.Title>
@@ -212,74 +213,7 @@ export function MovieDetail({ match }) {
 
       <hr className="mt-5" style={{ borderTop: "1px solid #5a606b" }}></hr>
 
-      <div className="row mt-3 mb-5">
-        <div className="col-md-8 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>ABOUT ME</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-            error earum perspiciatis praesentium sint ipsum provident blanditiis
-            pariatur necessitatibus voluptas, cum, atque iste eligendi autem,
-            culpa cupiditate placeat facilis repellat.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-            perspiciatis? Numquam, enim illo voluptatum neque facere aut sed ut
-            dolore nihil? Nulla sit, recusandae ea tenetur rerum deserunt sequi
-            earum?
-          </p>
-          <ul className="list-inline">
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-youtube"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="/" style={{ color: "#f4c10f" }}>
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-md-4 col-sm-6" style={{ color: "#5a606b" }}>
-          <h3>KEEP IN TOUCH</h3>
-          <ul className="list-unstyled">
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Address:
-                </strong>{" "}
-                city, state, country
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-map-marker-alt"></i> Phone:
-                </strong>{" "}
-                +01 00 00 00
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>
-                  <i className="fas fa-envelope"></i> Email:
-                </strong>{" "}
-                info@infomail.com
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
+      
     </div>
   );
 }
